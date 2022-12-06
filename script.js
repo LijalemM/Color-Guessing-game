8143b83daf7a98db57016af499324516bf5ad21c
Your Name <you@example.com>
2022-12-05 21:38:59 -0800

    second update

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
-let colors = ['blue','green','yellow','red','gold','purple']
+let colors = ['blue','green']
 let selected = [];
-let gameInput = [];
+let gameInput = " ";
 let gamerGuess = 0;
 let doneGussing = false;
 
 function generateRandomColor () {
-    selected = colors [Math.random()*colors.length]
+    selected = colors[Math.floor(Math.random()*colors.length)]
     while (doneGussing !=true){
-        gameInput = prompt('I am holding a color pan from these displayed: \n' + colors + '\n Guess the color pan I am holding!')
+        gameInput = prompt('I am holding a color from these listed below: \n' + colors + '\n Guess the color I am holding!')
+         /*if(typeof gameInput !="string"){
+            console.log("please use English characters only.")
+          }*/
         gamerGuess +=1;
         doneGussing = guessComparation ();
     }
     if (doneGussing){
-        displayer = document.getElementsByClassName("container"[0]);
+       let displayer = document.getElementsByClassName("container");
         displayer.style.backgroundcolor = selected;
         alert("Congratutlations You won the game!")
     }
 }
 function guessComparation (){
-    if (gameInput > colors[-1]){
-
-        if (gameInput.length > selected.length){
+    //if (gameInput > colors.length){
+ if (gamerGuess > 3){
+        console.log("sorry your given trial is up")
+     }
+ else if (gameInput.length > selected.length){
             alert ("Incorect Guess!\n" + " please try again! \n" + "Hint: My color words are shorter than your guess")
             return false;
         }
-        else if (gameInput.length < selected.length){
+     else if (gameInput.length < selected.length){
             alert ("Incorect Guess! \n" + "please try again! \n" + "Hint: My color words are higher than your guess word number")
             return false;
         }
-        else if (gameInput = selected){
+    else if (gameInput = selected){
             return true;
         }
 
-    }
+    //}
     else {
         alert("I don't recognize this color! check the list again! ")
         return false;
     }
 
 }
-
+addEventListener("load",generateRandomColor());
 
