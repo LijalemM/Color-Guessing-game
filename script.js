530ff213e5388712227f76f9895f595dfefa5324
Your Name <you@example.com>
2022-12-12 19:57:36 -0800

    updated

--- script.js
+++ script.js
@@ -1,56 +1,50 @@
-let colors = ['Green','Yellow','Red','Orange','Blue','Violet','Indigo','Silver','Brown','Gold',"Black", "Lime"]
-let randomSelected = [];
-let gamerInput = " ";
+let colors = ['Green','Yellow','Red','Orange','Blue','Violet','Indigo','Silver','Brown','Gold']
+let selected = [];
+let gameInput = " ";
 let gamerGuess = 0;
-let guessingDone = false;
+let doneGussing = false;
 
-//* this code is from stackoverFlow and class examples 
- function listColor  ()
-{
-    for(let key in colors)
-    {
-        let obj = colors[key];
-        let string = obj + "\n";
-        document.getElementById("displayText").innerHTML += string;
-    }
-}
-window.onload = function (){
-    document.getElementById("ListArray").onclick = listColor;
-    }
-
-function RandomColorgenerator (){
-    randomSelected = colors[Math.floor(math.random() * colors.length)]
-    while(guessingDone !=true){
-        gamerInput = " "
-        document.getElementById("inputText").value = gamerInput
+// this function generate randum value from the color listed in the colors array 
+function generateRandomColor () {
+    selected = colors[Math.floor(Math.random()*colors.length)]
+    while (doneGussing !=true){
+        gameInput = prompt('Guess a Color from Listed below: \n' + colors )
 
         gamerGuess +=1;
-        guessingDone = guessComparation ();
+        doneGussing = guessComparation ();
     }
-    if (guessingDone) {
-        alert("congrats You won the game" + "with " + gamerGuess + "guess!")
+    if (doneGussing){
+        //let displayer = document.getElementsByTagName("body");
+        //displayer.style.backgroundcolor === selected;
+        alert("Congratutlations You won the game!")
     }
 }
-
+// this function compare the gamer input and the randomly generated color
 function guessComparation (){
-    if (colors.indexOf(gamerInput) > -1){
-        if (gamerInput.length > randomSelected.length){
+
+    if (colors.indexOf(gameInput) > -1){
+
+    if (gamerGuess > 3){
+        console.log("sorry your given trial is up")
+     }
+    else if (gameInput.length > selected.length){
             alert ("Incorect Guess!\n" + " please try again! \n" + "Hint: My color words are shorter than your guess")
             return false;
         }
-        else if (gamerInput.length < randomSelected.length){
-            alert ("Incorect Guess! \n" + "please try again! \n" + "Hint: My color words are higher than your guess word number")
+    else if (gameInput.length < selected.length){
+            alert ("Incorect Guess! \n" + "please try again! \n" + "Hint: My color words are longer than your guess word number")
             return false;
         }
-        else if (gamerInput = randomSelected){
+    else if (gameInput = selected){
             return true;
         }
-        else 
-        {
-            alert("I don't recognize this color! check the list again! ")
-            return false;
-        }
-    
+
     }
+    else {
+        alert("I don't recognize this color! check the list again! ")
+        return false;
+    }
+
 }
-addEventListener("load",RandomColorgenerator());
+addEventListener("load",generateRandomColor());
+
